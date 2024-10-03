ba9b434963721236c614e1f3162e70c3dd7fdccc
AartHauk <andy111111111111111@gmail.com>
2024-10-03 00:46:40 -0600

    Inital Add of core

---
+++ core.h
@@ -0,0 +1,14 @@
+#ifndef ENGINE_CORE
+#define ENGINE_CORE
+
+#include <vulkan/vk_platform.h>
+#include <vulkan/vulkan_core.h>
+
+namespace engine {
+    
+    extern VkDevice DEVICE;
+    extern VkPhysicalDevice PHYSICAL_DEVICE;
+
+}
+
+#endif // ENGINE_CORE
